80aac750d6b2cbf0041547ef8c4cc7e4d03ca232
brianapeng <byinp222@gmail.com>
2022-07-07 21:47:36 -0400

    new change

---
+++ Homework Summer 2022/src/homework/summer/pkg2022/LinearSearch.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package homework.summer.pkg2022;
+
+/**
+ *
+ * @author brian
+ */
+public class LinearSearch {
+
+    static void LinearSearch() {
+        // write code here
+
+    }
+}
