f17caac4ec96e32917a879942825ef85085a9662
brianapeng <byinp222@gmail.com>
2022-07-14 11:14:29 -0400

    new edit

--- Homework Summer 2022/src/homework/summer/pkg2022/BinarySearch.java
+++ Homework Summer 2022/src/homework/summer/pkg2022/BinarySearch.java
@@ -5,13 +5,47 @@
  */
 package homework.summer.pkg2022;
 
+import java.util.Scanner;
+
 /**
  *
  * @author brian
  */
 public class BinarySearch {
-    static void BinarySearch(){
-        
-        //write code here
+
+    static void BinarySearch() {
+        Scanner s = new Scanner(System.in);
+        System.out.print("What is the size of your array? ");
+
+        int size = s.nextInt();
+        int[] arr = new int[size];
+
+        System.out.println("Please input the numbers");
+        for (int i = 0; i < size; i++) {
+            arr[i] = s.nextInt();
+        }
+
+        System.out.println("Input a number, and we will find the index for you.");
+        int key = s.nextInt();
+
+        int high = size - 1;
+        int low = 0;
+        int mid = (high + low) / 2;
+
+        while (low <= high) {
+            if (arr[mid] < key) {
+                low = mid + 1;
+            } else if (arr[mid] == key) {
+                System.out.println("Element is found at index: " + mid);
+                break;
+            } else {
+                high = mid - 1;
+            }
+            mid = (low + high) / 2;
+
+        }
+        if (low > high) {
+            System.out.println("Element is not found");
+        }
     }
 }
--- Homework Summer 2022/src/homework/summer/pkg2022/HomeworkSummer2022.java
+++ Homework Summer 2022/src/homework/summer/pkg2022/HomeworkSummer2022.java
@@ -15,12 +15,7 @@ public class HomeworkSummer2022 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        
-        
-        
-        
-        
+
         
     }
     
