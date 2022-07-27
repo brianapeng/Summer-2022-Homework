53c0101a660f2d58769edd5a23452a6d80b4c576
brianapeng <byinp222@gmail.com>
2022-07-27 10:55:44 -0400

    new edit

--- Homework Summer 2022/src/homework/summer/pkg2022/Palindrome.java
+++ Homework Summer 2022/src/homework/summer/pkg2022/Palindrome.java
@@ -1,17 +1,39 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
+ * To change this template file, choose Tools | Templatesz
  * and open the template in the editor.
  */
 package homework.summer.pkg2022;
+
 import java.util.Scanner;
 
 /**
  *
- * @author brian
+ * @author briana
  */
 public class Palindrome {
-    static 
-    
-    
+
+    public static boolean palindrome(String str) {
+        if (str.length() == 2) {
+            if (str.charAt(0) == str.charAt(1)) {
+                return true;
+            }
+            return false;
+        } else if (str.length() == 1) {
+            return true;
+        } else {
+            if (str.charAt(0) == str.charAt(str.length() - 1)) {
+                return palindrome(str.substring(1, str.length() - 1));
+            }
+            return false;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String str = s.next();
+
+    }
+
 }
